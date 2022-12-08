9b627dc4f143c17bfb033db774af06d2ccd0a353
Daniel <danielmartin971337@gmail.com>
2022-12-08 01:38:55 -0800

    made the database schema with all tables needed

---
+++ db/schema.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
+
+USE DATABASE employees_db;
+
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS employees;
+
+CREATE TABLE department(
+    id INT PRIMARY KEY NOT NULL,
+    depar_name VARCHAR(30) NOT NULL;
+);
+
+CREATE TABLE roles(
+    id INT PRIMARY KEY NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary INT NOT NULL,
+    department_id INT,
+);
+
+CREATE TABLE employee(
+    id INT PRIMARY KEY NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    job_id INT NOT NULL,
+    manager_id INT NOT NULL,
+);
